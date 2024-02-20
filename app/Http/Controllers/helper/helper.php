9038edd3fc4fc154f8dff9d0de89767fd184476d
Agadyr <kairambekovmadi@gmail.com>
2024-02-20 14:17:29 +0600

    grabed error response data by status

--- app/Http/Controllers/helper/helper.php
+++ app/Http/Controllers/helper/helper.php
@@ -22,7 +22,40 @@ class helper
     public static function getErrorResponse($request)
     {
         $status = $request->getResponse()->getStatusCode();
-        return response()->json();
+        return response()->json(helper::getErrorResponseDataByStatus($status),$status);
+    }
+
+    public static function getErrorResponseDataByStatus($status)
+    {
+        if ($status == '400') {
+            return [
+                "type" => "/problem/types/400",
+                "title" => "Bad Request",
+                "status" => 400,
+                "detail" => "The request is invalid."
+            ];
+        }else if ($status = '401') {
+            return [
+                "type" => "/problem/types/401",
+                "title" => "Unauthorized",
+                "status" => 401,
+                "detail" => "The header X-API-TOKEN is missing or invalid."
+            ];
+        }else if ($status == '403') {
+            return [
+                "type" => "/problem/types/403",
+                "title" => "Quota Exceeded",
+                "status" => 403,
+                "detail" => "You have exceeded your quota."
+            ];
+        }else if ($status = '503') {
+            return [
+                "type" => "/problem/types/503",
+                "title" => "Service Unavailable",
+                "status" => 503,
+                "detail" => "The service is currently unavailable."
+            ];
+        }
     }
 
 
