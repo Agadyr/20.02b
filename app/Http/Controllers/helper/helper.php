67bbc5da7736818cc7eee926a1a623ac0321ff78
Agadyr <kairambekovmadi@gmail.com>
2024-02-20 14:09:20 +0600

    implemented checkToken method

--- app/Http/Controllers/helper/helper.php
+++ app/Http/Controllers/helper/helper.php
@@ -7,6 +7,16 @@ use Illuminate\Support\Facades\DB;
 
 class helper
 {
-
+    public static function checkToken($tokenHeader)
+    {
+        $tokens = DB::connection('mysql')->select('select * from module_cdamp');
+        $isValid = false;
+        foreach ($tokens as $token) {
+            if ($token->token == $tokenHeader) {
+                $isValid = true;
+            }
+        }
+        return $isValid;
+    }
 
 }
